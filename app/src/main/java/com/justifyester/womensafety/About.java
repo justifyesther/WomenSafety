b5cd5ab6175261a917d0f83ed419e8542723a9cb
Justify Ester Pasaribu <44887209+justifyesther@users.noreply.github.com>
2022-03-09 12:51:27 +0700

    Initial commit

---
+++ app/src/main/java/com/justifyester/womensafety/About.java
@@ -0,0 +1,14 @@
+package com.justifyester.womensafety;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class About extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        setTitle("About");
+    }
+}
---
+++ app/src/main/java/com/justifyester/womensafety/Constant.java
@@ -0,0 +1,12 @@
+package com.justifyester.womensafety;
+
+public class Constant {
+    private static final String ROOT_URL = "http://192.168.43.172/emergency/v1/";
+    public static final String URL_CONTACT = ROOT_URL + "contact.php";
+    public static final String URL_MESSAGE = ROOT_URL + "contact2.php";
+    public static final String URL_CALL = ROOT_URL + "contact1.php";
+    public static final String URL_REMOVE = ROOT_URL + "remove.php";
+    public static final String URL_INSERT = ROOT_URL + "insert.php";
+    public static final String URL_LOGIN = ROOT_URL + "login.php";
+    public static final String URL_REGISTER = ROOT_URL + "register.php";
+}
